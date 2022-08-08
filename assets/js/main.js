66d8943ab2684ce7f7442aecdaa225f6585319a0
Nghia Dang <vannghiadang79@gmail.com>
2022-08-08 13:32:34 +0700

    day 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,56 @@ skillsHeaders.forEach(skillsHeader => {
         }
     });
 });
+
+// QUALIFICATION SECTION
+const qualificationTabs = document.querySelectorAll('[data-target]');
+const qualificationContents = document.querySelectorAll('[data-content]');
+
+qualificationTabs.forEach(clickTab => {
+    clickTab.addEventListener('click', () => {
+        const targetContent = document.querySelector(clickTab.dataset.target);
+
+        qualificationContents.forEach(qualificationContent => {
+            qualificationContent.classList.remove('qualification__active');
+        });
+        targetContent.classList.add('qualification__active');
+
+        qualificationTabs.forEach(qualificationTab => {
+            qualificationTab.classList.remove('qualification__active');
+        })
+        clickTab.classList.add('qualification__active');
+    });
+});
+
+// SERVICES SECTION
+const servicesBtns = document.querySelectorAll('.services__button');
+const servicesModals = document.querySelectorAll('.services__modal');
+const servicesModalCloses = document.querySelectorAll('.services__modal-close');
+
+servicesBtns.forEach((servicesBtn, index) => {
+    servicesBtn.addEventListener('click', () => {
+        servicesModals[index].classList.add('active-modal');
+    });
+});
+
+servicesModalCloses.forEach(servicesModalClose => {
+    servicesModalClose.addEventListener('click', () => {
+        servicesModals.forEach(servicesModal => {
+            servicesModal.classList.remove('active-modal');
+        })
+    });
+});
+
+// PORTFOLIO SECTION
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
